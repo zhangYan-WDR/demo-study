fcd55be6341f1c3bc0bd2a8b555a0708ff70e8ad
zhangyan <yan.zhang@zhishengai.com>
2023-03-14 17:54:30 +0800

    微信登录

---
+++ wx_login/src/main/java/com/zy/wx_login/WxLoginApplication.java
@@ -0,0 +1,13 @@
+package com.zy.wx_login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxLoginApplication.class, args);
+    }
+
+}
---
+++ wx_login/src/test/java/com/zy/wx_login/WxLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.wx_login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxLoginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
