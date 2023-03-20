3667ec3d902ed649312a709c89963b6220e52a9e
zhangyan <yan.zhang@zhishengai.com>
2023-03-20 16:38:01 +0800

    微信支付APIV3

---
+++ wxPayV3/src/main/java/com/zy/wxpayv3/WxPayV3Application.java
@@ -0,0 +1,13 @@
+package com.zy.wxpayv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxPayV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxPayV3Application.class, args);
+    }
+
+}
---
+++ wxPayV3/src/test/java/com/zy/wxpayv3/WxPayV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.wxpayv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxPayV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
